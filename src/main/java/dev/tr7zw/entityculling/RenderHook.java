ed793b10affd02ab211b56a0ef2ad3e1e70adee6
tr7zw <tr7zw@live.de>
2026-09-11 22:46:06 +0200

    Safe casting. Fix #8

--- src/main/java/dev/tr7zw/entityculling/RenderHook.java
+++ src/main/java/dev/tr7zw/entityculling/RenderHook.java
@@ -13,9 +13,10 @@ public final class RenderHook {
     // Has to be in a separate class otherwise the mixin won't work
     // (seems to be a bug in Mixin 0.7.11)
     public static <E extends Entity> void handle(Render<E> render, E entity, double x, double y, double z) {
-        @SuppressWarnings("unchecked")
-        RenderAccessor<E> accessor = ((RenderAccessor<E>) render);
-        accessor.callRenderName(entity, x, y, z);
+        if (render instanceof RenderAccessor) {
+            RenderAccessor<E> accessor = ((RenderAccessor<E>) render);
+            accessor.callRenderName(entity, x, y, z);
+        }
     }
 }
 
